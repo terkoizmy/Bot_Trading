1133f4b877f2346d78812467e75eedc944f0e42d
terkoizmy <56498701+terkoizmy@users.noreply.github.com>
2021-10-11 13:24:22 +0700

    api key
    need add api account

--- main.py
+++ main.py
@@ -27,10 +27,12 @@ logger.addHandler(file_handler)
 
 if __name__ == '__main__':  # Execute the following code only when executing main.py (not when importing it)
 
-    binance = BinanceClient("1a445ed5779c2f7633ec76ae32c65d9697da160118fdc57217b84448cd885941",
-                            "30871da1f2e4f70557a9cdc621a1fece4449186dda43693a1335028645e46edd",
+    # Input Your Binance API Key here
+    binance = BinanceClient("your-API-Key",
+                            "your-API-Secret",
                             testnet=True, futures=True)
-    bitmex = BitmexClient("8WQRYhSq0qamSkBnkzMk9QF0", "ZmHpW0gbQ2eBr0b89iiZPVbKMnvkvue69KeEGNDuhPllUu1I", testnet=True)
+    # Input Your bitmax API Key here
+    bitmex = BitmexClient("your-API-Key", "your-API-Secre", testnet=True)
 
     root = Root(binance, bitmex)
     root.mainloop()
